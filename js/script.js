4850091c3b500f21693afb6aca1a7181fcad4b09
Luca <lu.cucchi@gmail.com>
2020-03-17 13:53:59 +0100

    Progetto inizializzato

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// SCOPO DEL GIOCO: Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+// Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+// Layout base:
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
