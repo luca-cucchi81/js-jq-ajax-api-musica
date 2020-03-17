d0045819bf53c8da603664dae200b6b91d6dfd8c
Luca <lu.cucchi@gmail.com>
2020-03-17 18:14:36 +0100

    BONUS: completo

--- js/script.js
+++ js/script.js
@@ -4,27 +4,42 @@
 // Chiamata: https://flynn.boolean.careers/exercises/api/array/music
 // Layout base:
 // https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
+$(document).ready(function () {
+    $.ajax({
+        url:'https://flynn.boolean.careers/exercises/api/array/music',
+        method: 'GET',
+        success: function (data) {
+            var albums = data.response;
+            getAlbums(albums)
+        },
+        error: function (errore) {
+            alert('Attenzione è presente un errore');
+        }
+    });
 
-$.ajax({
-    url:'https://flynn.boolean.careers/exercises/api/array/music',
-    method: 'GET',
-    success: function (data) {
-        var albums = data.response;
-        getAlbums(albums)
-    },
-    error: function (errore) {
-        alert('Attenzione è presente un errore');
-    }
-});
+    $('#generi').change(function () {
+        var genereAttuale = $(this).val();
+        if (genereAttuale == '') {
+            $('.cd').show();
+        }else {
+            $('.cd').each(function () {
+                if (genereAttuale.toLowerCase() == $(this).data('genre').toLowerCase()) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
 
+                }
+            })
+        }
+    })
+})
 
 // <<..FUNZIONI..>>
 function getAlbums(albumsList){
     albumsList.forEach(function (cd) {      //trovato su w3school per selezionare i singoli oggetti dell'array dell'API
         var source = $('#card-template').html();
         var template = Handlebars.compile(source);
-        var dischi = cd ;                  //per matchare le chiavi ho dovuto dar loro lo stesso nome anche nell'HTML
-        var disco = template(dischi);
+        var disco = template(cd);
         $('.container').append(disco);
         console.log(cd);
     });
